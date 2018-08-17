3878d3e6f6c3100b4edcc89d61eaa625e1b9d030
Anshul Malik <malikanshul29@gmail.com>
2018-08-17 19:32:14 +0530

    more cleaner

--- services/seller.js
+++ services/seller.js
@@ -7,29 +7,29 @@ const models = require('../models');
 
 module.exports = {
 
-  createSeller: (seller) => {
-    return bcrypt.hash(seller.password, config.bcryptSaltRounds).then(hash => {
-      seller.password = hash;
-      return models.seller.create(seller).then(seller => seller.dataValues);
-    });
+  createSeller: async seller => {
+    seller.password = await bcrypt.hash(seller.password, config.bcryptSaltRounds);
+    const { dataValues } = await models.seller.create(seller);
+    return dataValues;
   },
 
-  updateSeller: params => {
-    return models.seller.findOne({
+  updateSeller: async params => {
+    const seller = await models.seller.findOne({
       where: {
         id: params.id
       }
-    }).then(async seller => {
-      if (!seller) {
-        throw ('No such seller Exist');
-      }
-
-      // update password
-      if (params.password) {
-        params.password = await bcrypt.hash(params.password, config.bcryptSaltRounds);
-      }
-
-      return seller.updateAttributes(params).then(seller => seller.dataValues);
     });
+
+    if (!seller) {
+      throw ('No such seller Exist');
+    }
+
+    // update password
+    if (params.password) {
+      params.password = await bcrypt.hash(params.password, config.bcryptSaltRounds);
+    }
+
+    const { dataValues } =  await seller.updateAttributes(params);
+    return dataValues;
   }
 };
