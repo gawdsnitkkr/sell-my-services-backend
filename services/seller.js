1e2254c72e46ff7bc3cfe78430849412d94c606c
Anshul Malik <malikanshul29@gmail.com>
2018-08-17 09:06:43 +0530

    code cleanup

--- services/seller.js
+++ services/seller.js
@@ -8,46 +8,28 @@ const models = require('../models');
 module.exports = {
 
   createSeller: (seller) => {
-    return new Promise((resolve) => {
-      bcrypt.hash(seller.password, config.bcryptSaltRounds).then((hash) => {
-        seller.password = hash;
+    const hash = bcrypt.hashSync(seller.password, config.bcryptSaltRounds);
+    seller.password = hash;
 
-        // insert seller info to db
-        models.seller.create(seller).then(seller => {
-          resolve(seller.dataValues);
-        });
-      });
-    });
+    return models.seller.create(seller).then(seller => seller.dataValues);
   },
 
-  updateSeller: (params) => {
-    return new Promise((resolve, reject) => {
-      models.seller.findOne({
-        where: {
-          id: params.id
-        }
-      }).then(seller => {
-        if (seller) {
-          // update password
-          if (params.password) {
-            bcrypt.hash(params.password, config.bcryptSaltRounds)
-              .then((hash) => {
-                params.password = hash;
-                seller.updateAttributes(params)
-                  .then(seller => {
-                    resolve(seller.dataValues);
-                  });
-              });
-          } else {
-            seller.updateAttributes(params)
-              .then(seller => {
-                resolve(seller.dataValues);
-              });
-          }
-        } else {
-          reject('No such seller Exist');
-        }
-      });
+  updateSeller: params => {
+    return models.seller.findOne({
+      where: {
+        id: params.id
+      }
+    }).then(seller => {
+      if (!seller) {
+        throw ('No such seller Exist');
+      }
+
+      // update password
+      if (params.password) {
+        params.password = bcrypt.hashSync(params.password, config.bcryptSaltRounds);
+      }
+
+      return seller.updateAttributes(params).then(seller => seller.dataValues);
     });
   }
 };
